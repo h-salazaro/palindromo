779f1dca7da9753b1682e98af2233fabf09e96de
Héctor Salazar <h.salazar.olivares@accenture.com>
2016-06-23 16:41:17 -0500

    Added rspec

--- spec/palindromo_spec.rb
+++ spec/palindromo_spec.rb
@@ -6,16 +6,10 @@ describe Palindromo do
 
   describe '#process' do
 
-    let(:ana) { "ana" }
-    let(:tom) { "tom" }
     let(:input) { "anitalavalatina" }
 
     it 'verifies word is a string' do
       expect(input).to be_a String
-    end
-
-    it 'verifies tom is not a palindrome' do
-      expect(tom.reverse).not_to eq "tom"
     end
 
     it 'returns true with a valid palindrome' do
